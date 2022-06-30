95c387278c821fe831b028cfe1a8bf48750bf38e
Krystal Lopez <lopezkrystal10@gmail.com>
2022-06-30 10:54:48 -0700

    added routes and controllers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,56 @@
 class ProductsController < ApplicationController
+
+  def index
+    products = Product.all
+    render json: products.as_json
+  end 
+
+  def show
+    product_id = params["id"]
+    product = Product.find_by(id: product_id)
+    render json: product.as_json
+  end 
+
+  def create
+    product = Product.new( 
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
+      
+    )
+    product.save
+    render json: product.as_json
+  end
+
+  def update 
+    product_id = params["id"]
+    product = Product.find_by(id: product_id)
+
+    product.name = params["name"] || product.name
+    product.price = params["price"] || product.price
+    product.image_url = params["image_url"] || product.image_url
+    product.description = params["description"] || product.description
+  
+  
+    product.save
+    render json: product.as_json
+  end
+
+  def destroy 
+    product_id = params["id"]
+    product = Product.find_by(id: product_id)
+    product.destroy
+    render json: {message: "Product has been successfully deleted."}
+  end
+
 end
+
+
+
+
+
+
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+ 
+  get "/products" => "products#index"
+  get "/products" => "products#show"
+  post "/products" => "products#create"
+  patch "/products/:id" => "products#update"
+  delete "/products/:id" => "products#destroy"
 end
