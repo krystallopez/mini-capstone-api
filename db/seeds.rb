555add2a4ac03e42d6ff0a3d4fdbb7263c43d912
Krystal Lopez <lopezkrystal10@gmail.com>
2022-06-30 09:21:05 -0700

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Product.create({ name: "Destiny 2", price: 59.99, image_url: "https://bungiestore.com/media/catalog/product/M/i/Microsoft_Stardust_1x1BoxArt_2160x2160_EN_e2d1e934-0452-4925-b559-662b3325d1e8.png-v_1600880545?quality=80&bg-color=255,255,255&fit=bounds&height=265&width=265&canvas=265:265", description: "Destiny 2 is a shared world, first person shooter game. The game also combines of RPG genre into one phenomenal experience. Create your guardian join the epic battle between the darkness and the light!" })
+
+Product.create({ name: "Mass Effect: Legendary Edition", price: 59.99, image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSAWBTV4W7Hm1kCwlS5fnRKPBpdTAOqsg9Uw&usqp=CAU", description: "The Mass Effect Legendary Edition includes single-player based content and over 40 DLC from the entire Mass Effect Trilogy. This include promo weapons, armors, packs, and it has been remastered for 4K Ultra HD." })
+
+Product.create({ name: "Horizon Zero Dawn", price: 59.99, image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpc_km5UU9oqrEM2YI5_wz59p3XErACWz_6haWMsAqqkXqVI0frt5Ryfdy6z0AbK2msuI&usqp=CAU", description: "Horizon Zero Dawn, follows Aloy, a young huntress in a world that has been overrun by machines. Play as Aloy as she sets out to uncover her past." })
+
+Product.create({ name: "Star Wars: Jedi Fallen Order", price: 19.99, image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmebWqvp51fffPxja2ZXN4IsgYkwOmxYsuGg&usqp=CAU", description: "Star Wars: Jedi Fallen Order is action-packed adventure game. Set after the events of Episode III: Revenge of the Sith, you play as former Jedi padawan Cal Kestis. Develop your force abilities, hone your lightsaber techniques, and explore ancient mysteries of a long-losy civilization - all while trying to stay one step in front of Darth Vader and his deadly inquisitors." })
+
+puts "Done."
