75f24090d812474042213fdb8f6196d73becbb93
Krystal Lopez <lopezkrystal10@gmail.com>
2022-07-05 18:33:59 -0700

    changed description column type from string to text

---
+++ db/migrate/20220706013032_make_changes_to_products.rb
@@ -0,0 +1,6 @@
+class MakeChangesToProducts < ActiveRecord::Migration[7.0]
+  def change
+
+    change_column :products, :description, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_30_145752) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_06_013032) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_30_145752) do
     t.string "name"
     t.integer "price"
     t.string "image_url"
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
